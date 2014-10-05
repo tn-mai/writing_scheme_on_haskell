a7b848cdc83a7e54df6273252fc43762e28237b3
tn_mai <rabitfiver@gmail.com>
2014-10-05 18:56:29 +0900

    Add the space parser.

--- simple-parser.hs
+++ simple-parser.hs
@@ -1,11 +1,14 @@
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse (spaces >> symbol) "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
 
