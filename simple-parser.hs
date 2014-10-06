ba6fbc0412e08dc30262cb60da056e158a707732
tn_mai <rabitfiver@gmail.com>
2014-10-07 05:56:01 +0900

    Implement the simple evaluator.

--- simple-parser.hs
+++ simple-parser.hs
@@ -119,12 +119,19 @@ spaces = skipMany1 space
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
-readExpr :: String -> String
+-- | Evaluate LispVal.
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Float _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
+eval val = val
+
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-  Left err -> "No match: " ++ show err
-  Right val -> "Found value: " ++ show val
+  Left err -> String $ "No match: " ++ show err
+  Right val -> val
 
 main :: IO ()
-main = do
-  args <- getArgs
-  putStrLn . readExpr $ args !! 0
+main = getArgs >>= print . eval . readExpr . head
