d044a14bce0e13ae5f9665c496c3ab726d0c8f21
tn_mai <rabitfiver@gmail.com>
2014-10-10 19:23:48 +0900

    Fix an error of the shadowing the existing binding.

--- simple-parser.hs
+++ simple-parser.hs
@@ -25,7 +25,7 @@ showVal (Float contents) = show contents
 showVal (Bool True) = "#t"
 showVal (Bool False) = "#f"
 showVal (List contents) = "(" ++ unwordsList contents ++ ")"
-showVal (DottedList head tail) = "(" ++ unwordsList head ++ ", " ++ showVal tail ++ ")"
+showVal (DottedList h t) = "(" ++ unwordsList h ++ ", " ++ showVal t ++ ")"
 
 unwordsList :: [LispVal] -> String
 unwordsList = unwords . map showVal
